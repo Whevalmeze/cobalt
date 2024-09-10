c114dfbd82670863530a0e25feb04f8f8446b326
Whevalmeze <princegbefavor@gmail.com>
2024-09-10 02:29:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Cobalt",
+  description: "The Power of Finance",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className + " bg-custom-blue text-base text-custom-grey"}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './components/Nav'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className=''>
+      <Nav />
+    </div>
+  )
+}
+
+export default page
